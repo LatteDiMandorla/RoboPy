b17bfacf08fedf1931769de2f6ea8cdb462f8949
Sderr12 <andrea.sidero6@gmail.com>
2025-05-03 18:38:01 +0200

    Major changes to server.c

--- server/src/server.c
+++ server/src/server.c
@@ -22,7 +22,6 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    /* Creation of server address. */
 
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
@@ -30,14 +29,12 @@ int main() {
     server_address.sin_addr.s_addr = INADDR_ANY;
     memset(server_address.sin_zero, 0, sizeof(server_address.sin_zero)); 
 
-    // Associa il socket all'indirizzo
     int bind_status = bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address));
     if (bind_status == -1) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // Mette il server in ascolto
     int listen_status = listen(server_socket, 5);
     if (listen_status == -1) {
         perror("Listen failed");
@@ -45,36 +42,46 @@ int main() {
     }
 
     printf("Server in ascolto sulla porta 8080...\n");
+    
 
-    // Accetta una connessione dal client
-    struct sockaddr_in client_address;
-    socklen_t client_address_len = sizeof(client_address);
-    int client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_len);
-    if (client_socket == -1) {
-        perror("Accept failed");
-        exit(EXIT_FAILURE);
+    /* Accepting clients comunication */
+
+    while (1)
+    {
+       struct sockaddr_in client_address;
+       socklen_t client_address_len = sizeof(client_address);
+       
+       int client_scoket = accept(server_socket, (struct sockaddr *)& client_address, &client_address_len);
+       if (client_socket == -1)
+       {
+          perror("Accept failed!");
+          continue;
+
+       }
+       
+       /* INET_ADDRSTRLEN is a costant variable, defined into <arpa/inet.h>, it's equal
+        * to 16, and it's the maximum amount necessary to store an IPv4 adress as a string. */ 
+
+       char client_ip[INET_ADDRSTRLEN];
+
+       /* Converts a binary IP, stored in struct sockaddr_in, to a string. */ 
+
+       inet_ntop(AF_INET, &(client_address.sin_addr), client_ip, INET_ADDRSTRLEN);
+       
+       char buffer[BUFFER_SIZE];
+
+       int recv_status = recv(client_scoket, buffer, sizeof(buffer) - 1, 0);
+       if (recv_status > 0)
+       {
+          buffer[recv_status] = '\0';
+          
+
+       }
+
+       close(client_socket);
+
     }
-
-    // Comunica con il client
-    char buffer[BUFFER_SIZE];
-    int recv_status = recv(client_socket, buffer, sizeof(buffer), 0);
-    if (recv_status == -1) {
-        perror("Receive failed");
-        exit(EXIT_FAILURE);
-    }
-    printf("Client: %s\n", buffer);
-
-    // Risposta al client
-    char *message = "Ciao, client! Ho ricevuto il tuo messaggio.";
-    int send_status = send(client_socket, message, strlen(message), 0);
-    if (send_status == -1) {
-        perror("Send failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Chiude la connessione
-    close(client_socket);
-    close(server_socket);
+       close(server_socket);
 
     return 0;
 }
